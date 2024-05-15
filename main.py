c0e7cc9d8a99f0cd93439fb2809a59f7121fcdc7
qrcodr_ <rafael.sm10@icloud.com>
2024-05-15 13:32:43 -0300

    feat: add text2speech function
    This function converts text to audi, generating an audio file (flac) in the ./output folder.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import os
+
+import requests
 from dotenv import find_dotenv, load_dotenv
 from langchain import LLMChain, OpenAI
 from langchain_core.prompts import PromptTemplate
 from transformers import pipeline
 
 load_dotenv(find_dotenv())
+HUGGINGFACEHUB_API_TOKEN = os.getenv('HUGGINGFACEHUB_API_TOKEN')
 
 # Image to text
 def img2text(url):
@@ -25,8 +29,17 @@ def generate_story(scenario):
   story_llm = LLMChain(llm=OpenAI(model_name='gpt-3.5-turbo', temperature=1), prompt=prompt, verbose=True)
   story = story_llm.predict(scenario=scenario)
   return story
+
+# Text to epeech
+def text2speech(message):
+  API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
+  headers = {"Authorization": f"Bearer {HUGGINGFACEHUB_API_TOKEN}"}
+  paylods = {'inputs': message}
+  response = requests.post(API_URL, headers=headers, json=paylods)
+  with open('./output/audio.flac', 'wb') as file:
+    file.write(response.content)
  
 if __name__ == '__main__':
   description = img2text('./data/img.jpg')
   story = generate_story(description)
-  print(f'\nSTORY: {story}')
+  text2speech(story)
